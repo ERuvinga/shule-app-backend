c270cea36c9bf75516f36a0e7a7c71002b55a821
ERuvinga <ruvingaelie@gmail.com>
2023-08-21 22:56:38 +0200

    Connect mongodb to App

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 
-// notre Application EPRESS
+// include connect mongoose to cluster mangodb
+require("./db/mongo_db");
+
 
 const express = require('express');
 const app = express();  // methode express
+const userShema = require('./Models/test');
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,18 +14,20 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/test', (req, res) => {
+app.use('/test', (req, res) => { // fecting user of Test Api
 
-    const datas = [
-        {
-            _id: '1234',
-            title: "Node Api test root",
-            description: "Web dev",
-            Autor: "Elie_Ruvinga"
-        }
-    ]
-    res.json(datas);
-    res.status(200);
+    let datas;
+    userShema.find()
+    .then(allUsers => {
+         datas = {...allUsers};
+         res.json(datas);
+         res.status(200);
+    })
+    .catch((error)=> {
+        console.log(error);
+        res.json({message:"Error server"});
+        res.status(200);
+    });
 });
 
 
--- db/mongo_db.js
+++ db/mongo_db.js
@@ -2,7 +2,7 @@
 // connexion node Api to cluster Mongo
 require('dotenv').config(); // config and import .env file 
 const mongoose = require("mongoose");
-const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PWD}@neema.mke4ipc.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PWD}@neema.mke4ipc.mongodb.net/shule-App`
 const connexion_options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
