0ba39646a902fee9a3d05c5d6804a380889071d8
ERuvinga <ruvingaelie@gmail.com>
2023-08-21 19:48:29 +0200

    start projet

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// notre Application EPRESS
+
+const express = require('express');
+const app = express();  // methode express
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+    next();
+});
+
+app.use('/test', (req, res) => {
+
+    const stuff = [
+        {
+            _id: '1234',
+            title: "mon article",
+            description: "Camera",
+            imageUrl: "",
+            UserId: "elie"
+        }
+    ]
+    res.json(stuff);
+    res.status(200);
+});
+
+
+
+module.exports = app;
